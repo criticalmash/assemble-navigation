ada190cae35738bd7690eee05d360f527c8f5a0b
John O'Donnell <john@criticalmash.com>
2014-03-09 15:25:24 -0400

    Building out Lib/navigation and adding to the test

--- README.md
+++ README.md
@@ -19,3 +19,5 @@ The site builder can then pass that navigation object to a template/partial to b
 The plugin would have sensible defaults that can be overridden in the task config, front-matter, etc. By default, the plugin might create one navigation called "main" and place all pages into it, sorted by page title. The site builder could override this default by adding a new navigation called "footer", and then update the front-matter of some pages to indicate that the page belongs in "footer" instead of "main". A similar method could be used to specify a custom sort order, use an alternative title in the menu, etc.
 
 The data passed to each page's template would also be altered for each page to indicate the current page and it's parents.
+
+[More details](https://github.com/assemble/assemble/issues/462)
--- test/main_test.js
+++ test/main_test.js
@@ -7,10 +7,21 @@
  * Licensed under the MIT license.
  */
 
+// node builtins
+var path = require('path');
+
+// npm modules
 var expect = require('chai').expect;
+var grunt = require('grunt');
 
 var navigation = require('../lib/navigation');
 
+// stolen from Assembles collection_test.js
+var getJson = function(file) {
+  return grunt.file.readJSON(path.join('./test/fixtures/scratch/', file));
+};
+
+// the tests
 describe('assemble-contrib-navigation', function() {
 
   before(function(){
@@ -20,6 +31,13 @@ describe('assemble-contrib-navigation', function() {
   it('should do something awesome', function() {
     var expected = 'assemble-contrib-navigation';
     var actual = 'assemble-contrib-navigation';
+    expect(actual).to.eql(expected);
+  });
+
+  it('navigation.build() should return a navigation object', function() {
+    var expected = {main:[]};
+    var input = getJson('sparse.json');
+    var actual = navigation.build(input);
     expect(actual).to.eql(expected);
   });
 
