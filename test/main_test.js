6b735ffcf0357c79d6f2f576d5b756ed2768c53c
John O'Donnell <john@criticalmash.com>
2014-03-10 11:03:15 -0400

    Changed how object is formatted. Added some config functions

--- test/main_test.js
+++ test/main_test.js
@@ -13,12 +13,13 @@ var path = require('path');
 // npm modules
 var expect = require('chai').expect;
 var grunt = require('grunt');
+var _ = require('lodash');
 
 var navigation = require('../lib/navigation');
 
 // stolen from Assembles collection_test.js
 var getJson = function(file) {
-  return grunt.file.readJSON(path.join('./test/fixtures/scratch/', file));
+  return grunt.file.readJSON(path.join('./test/fixtures/', file));
 };
 
 // the tests
@@ -34,11 +35,57 @@ describe('assemble-navigation', function() {
     expect(actual).to.eql(expected);
   });
 
-  it('navigation.build() should return a navigation object', function() {
-    var expected = {main:[]};
-    var input = getJson('sparse.json');
+  // it('navigation.build() should populate options', function() {
+  //   var expected = {main:{items:[]}};
+  //   var input = getJson('scratch/sparse.json');
+  //   var actual = navigation.build(input);
+  //   expect(navigation.options).to.eql(expected);
+  // });
+
+  // it('custom-nav-config', function() {
+  //   var expected = getJson('expected/custom-nav-config.json');
+  //   var input = getJson('inputs/custom-nav-config.json');
+  //   var actual = navigation.build(input);
+  //   //expect(navigation.options).to.eql(expected.assemble.options);
+  //   expect(navigation.options).to.include.key('foot');
+  // });
+
+  it('Should add pages to main', function() {
+    var expected = getJson('expected/custom-nav-config.json');
+    var input = getJson('inputs/assemble-post-pages.json');
     var actual = navigation.build(input);
-    expect(actual).to.eql(expected);
+    //expect(navigation.options).to.eql(expected.assemble.options);
+    expect(actual).to.include.key('main');
   });
+
+  it('Should add pages to asside', function() {
+    var expected = getJson('expected/custom-nav-config.json');
+    var input = getJson('inputs/assemble-post-pages.json');
+    // make asside the default menu
+    input.navigation.asside.default = true;
+    var actual = navigation.build(input);
+    //expect(navigation.options).to.eql(expected.assemble.options);
+    //expect(actual).to.include.key('main');
+    expect(navigation.defaultMenu).to.eql('asside');
+    expect(actual.asside.items).to.have.length.above(2);
+  });
+
+  it('About should be in footer menu', function() {
+    var expected = getJson('expected/custom-nav-config.json');
+    var input = getJson('inputs/assemble-post-pages.json');
+    // set menu option for about.hbs
+    _(input.pages).find({basename:'about'}).data['menu'] = 'footer';
+
+    var actual = navigation.build(input);
+    expect(actual.footer.items).to.have.length.above(0);
+  });
+
+
+  // it('navigation.build() should return a navigation object', function() {
+  //   var expected = {main:[]};
+  //   var input = getJson('sparse.json');
+  //   var actual = navigation.build(input);
+  //   expect(actual).to.eql(expected);
+  // });
 
 });
